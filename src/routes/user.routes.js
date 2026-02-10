d49196e91a9d55b8175d407edadd2412a9e3a16a
Sazal <sazalpathania.777@gmail.com>
2026-02-10 12:39:32 +0530

    Add user profile and watch history routes, including account update and avatar/cover image management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  userUpdateAvatar,
+  userUpdateCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -24,5 +31,25 @@ userRouter.post("/login", loginUser);
 //secured routes
 userRouter.post("/logout", verifyJWT, logoutUser);
 userRouter.post("/refresh-token", refreshAccessToken);
+userRouter.post("/change-password", verifyJWT, changeCurrentPassword);
+userRouter.post("/get-user", verifyJWT, getCurrentUser);
+userRouter.patch("/upadate-account", verifyJWT, updateAccountDetails);
+
+userRouter.patch(
+  "/avatar",
+  verifyJWT,
+  upload.single("avatar"),
+  userUpdateAvatar
+);
+
+userRouter.patch(
+  "/coverImage",
+  verifyJWT,
+  upload.single("coverImage"),
+  userUpdateCoverImage
+);
+
+userRouter.get("/c/:username", verifyJWT, getUserChannelProfile);
+userRouter.get("/watch-history", verifyJWT, getWatchHistory);
 
 export default userRouter;
